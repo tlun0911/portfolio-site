61580975f1bbfaee571237b026d54a6eff4d339f
tlun0911 <tlun0911@gmail.com>
2024-06-25 10:13:22 -0400

    Fixed font size

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -17,8 +17,8 @@ const Story = () => {
       </div>
       <div className="container mx-auto px-6 md:px-12 lg:px-20">
         <div className="text-center">
-          <h3 className="text-2xl font-semibold text-white">How I Got Here</h3>
-          <p className="text-white mt-4">
+          <h3 className="text-2xl font-bold text-gray-200">How I Got Here</h3>
+          <p className="text-gray-200 text-lg mt-4">
             I originally went to school for computer science, but ended up
             changing my major and graduated with a degree in criminal justice.
             Earlier this year, I was unforuntately laid off from my job and
